47a9b5a9c7c90656df651e8d3166b81043b9c85c
Syed Huzaifa <68459184+SyedHuzaifa007@users.noreply.github.com>
2023-12-09 15:36:00 +0500

    update

---
+++ WayToGlobe/authentication/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                # Redirect to a success page or homepage
+                return render(request, 'success.html')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                # Redirect to a success page or homepage
+                return render(request, 'success.html')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'signup.html', {'form': form})
