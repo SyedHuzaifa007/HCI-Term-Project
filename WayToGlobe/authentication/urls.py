eae7647da5028fac9516d3a41e39f37c5ed5b7a9
AdolfTrumpler <116947935+AdolfTrumpler@users.noreply.github.com>
2023-12-12 22:18:56 +0500

    login issue and email alignment

--- WayToGlobe/authentication/urls.py
+++ WayToGlobe/authentication/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.login_view, name='login'),
     path('login/', views.login_view, name='login'),
     path('signup/', views.signup_view, name='signup'),
     path('password_reset',views.forgot,name="password_reset"),
--- WayToGlobe/authentication/views.py
+++ WayToGlobe/authentication/views.py
@@ -17,16 +17,26 @@ def login_view(request):
                 return redirect('main_interface')
     context={'loginform':form}
     return render(request, 'login.html',context=context)
-
+import re
 def signup_view(request):
-    form=CreateUserForm()
+    form = CreateUserForm()
     if request.method == 'POST':
         form = CreateUserForm(request.POST)
         if form.is_valid():
-          form.save()
-          return redirect('login')
-    context={'registration':form}
-    return render(request, 'signup.html',context=context)
+            # Process additional validation on the server side
+            username = form.cleaned_data.get('username')
+            # Add more validation for other fields
+
+            if not re.match("^[a-zA-Z0-9]{4,}$", username):
+                form.add_error('username', 'Invalid username format')
+                return render(request, 'signup.html', {'registration': form})
+
+            # Process signup logic here
+            form.save()
+            return redirect('login')
+
+    context = {'registration': form}
+    return render(request, 'signup.html', context=context)
 
 from django.contrib.auth.forms import PasswordResetForm
 from django.contrib.auth.views import PasswordResetView
